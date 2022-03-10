b69df320704c46e398c175c10d7f8389ee8512e4
Harshit Gupta <harshitgupta.gh53@gmail.com>
2022-03-11 00:21:05 +0530

    Authentication Done

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express       = require("express"),
       bodyParser    = require("body-parser"),
       mongoose      = require("mongoose"),
       passport      = require("passport"),
-      LocalStrategy = require("passport-local");
-const res = require("express/lib/response");
+      LocalStrategy = require("passport-local"),
+      User          = require("./models/user");
 
 const app = express();
 
@@ -15,19 +15,60 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 
-app.get("/signin", function(req, res) {
-    res.render("index/signin");
-})
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Attack on titan is the best anime ever",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.get("/signup", function(req, res) {
     res.render("index/signup");
-})
+});
+
+app.post("/signup", function(req, res) {
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+        if(err) {
+            console.log(err);
+            return res.render("index/signup");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/contacts")
+        });
+    });
+});
+
+app.get("/signin", function(req, res) {
+    res.render("index/signin");
+});
+
+app.post("/signin", passport.authenticate("local",
+    {
+        successRedirect: "/contacts",
+        failureRedirect: "/signin"
+    }), function(req, res) {
+});
 
 app.get("/contacts", function(req, res) {
     res.render("index/contacts");
-})
+});
+
+app.get("/signout", function(req, res){
+    req.logout();
+    res.redirect("/signin");
+});
+
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, process.env.IP, function() {
     console.log("Contacts app server has started on port" + port);
-})
+});
